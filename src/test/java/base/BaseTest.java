572b590488d3c606c13b2d98485be34aa90316de
minijamigara <mahdse201f002@student.nibm.lk>
2023-06-07 05:01:13 +0530

    Finding web elements

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class BaseTest {
     private WebDriver driver;
 
@@ -16,8 +18,12 @@ public class BaseTest {
         driver.manage().window().setSize(new Dimension(375, 812)); // assign a size to the window
         //System.out.println(driver.getTitle());
 
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
         WebElement inputLink = driver.findElement(By.linkText("Inputs"));
         inputLink.click();
+
         //driver.close();//close the browser
         //driver.quit() //close any browser window opened
     }
