eed8c4a431876b7298b81c488abe475e0a30cfe3
minijamigara <mahdse201f002@student.nibm.lk>
2023-06-07 04:53:17 +0530

    Access input field by link text

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,9 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
@@ -12,8 +14,11 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().setSize(new Dimension(375, 812)); // assign a size to the window
-        System.out.println(driver.getTitle());
-        driver.close();//close the browser
+        //System.out.println(driver.getTitle());
+
+        WebElement inputLink = driver.findElement(By.linkText("Inputs"));
+        inputLink.click();
+        //driver.close();//close the browser
         //driver.quit() //close any browser window opened
     }
 
